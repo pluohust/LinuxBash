f28f615e18898d9dffffbf5d22a5570f7d5f4aa0
unknown <>
2018-01-04 16:01:29 +0800

    第一次上传

---
+++ Debian_Simplified_Install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#精简安装Debian9后的脚本
+
+echo 'Update the update source'
+
+echo 'deb http://mirrors.163.com/debian/ stretch main non-free contrib
+deb http://mirrors.163.com/debian/ stretch-updates main non-free contrib
+deb http://mirrors.163.com/debian/ stretch-backports main non-free contrib
+deb-src http://mirrors.163.com/debian/ stretch main non-free contrib
+deb-src http://mirrors.163.com/debian/ stretch-updates main non-free contrib
+deb-src http://mirrors.163.com/debian/ stretch-backports main non-free contrib
+deb http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib
+deb-src http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib'
+
+echo 'deb http://mirrors.163.com/debian/ stretch main non-free contrib
+deb http://mirrors.163.com/debian/ stretch-updates main non-free contrib
+deb http://mirrors.163.com/debian/ stretch-backports main non-free contrib
+deb-src http://mirrors.163.com/debian/ stretch main non-free contrib
+deb-src http://mirrors.163.com/debian/ stretch-updates main non-free contrib
+deb-src http://mirrors.163.com/debian/ stretch-backports main non-free contrib
+deb http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib
+deb-src http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib' > /etc/apt/source.list
+
+echo 'Update...'
+
+apt update
+apt -y upgrade
+
+echo 'Install china fonts'
+
+apt -y install ttf-wqy-*
+
+echo 'Install xfce4!'
+
+apt -y install xfce4 xfce4-terminal
+
+echo 'Choose utf-8'
+
+dpkg-reconfigure locales
